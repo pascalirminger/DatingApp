348653fe2e716da66f306cc7af3d5f9e97f33aaf
Pascal Irminger <pascal.irminger@gmail.com>
2018-08-25 17:10:31 +0200

    End of Section 16

--- DatingApp-SPA/src/app/lists/lists.component.ts
+++ DatingApp-SPA/src/app/lists/lists.component.ts
@@ -3,7 +3,7 @@ import { User } from '../_models/user';
 import { Pagination, PaginatedResult } from '../_models/pagination';
 import { AuthService } from '../_services/auth.service';
 import { UserService } from '../_services/user.service';
-import { ActivatedRoute } from '../../../node_modules/@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
